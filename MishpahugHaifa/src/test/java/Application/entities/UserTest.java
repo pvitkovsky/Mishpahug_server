9346c85d5086eccbcf063c00663e07f58c38fa8a
pvitkovsky <vitkovsky1337@gmail.com>
2019-02-11 14:47:46 +0200

    Update UserTest.java

--- MishpahugHaifa/src/test/java/Application/entities/UserTest.java
+++ MishpahugHaifa/src/test/java/Application/entities/UserTest.java
@@ -27,35 +27,13 @@ import Application.repo.UserRepository;
 @Transactional
 public class UserTest {
 
-	// TODO: create-delete author and book please
-
-//	private static final String GERMANY = "Germany";
-//	private static final String RED_SEA = "Red Sea";
-//	private static final List<Integer> JOYCEMARKS = Arrays.asList(new Integer[]{5, 5, 4});
-//	private static final AuthorId JOYCE = new AuthorId("James", "Joyce");
-//	private static final Author[] authorArray = { new Author(JOYCE, JOYCEMARKS) };
-//	private static final Set<Author> AUTHORS = new HashSet<Author>(Arrays.asList(authorArray));
-//	private static final Publisher RED_SEA_GERMANY = new Publisher(RED_SEA, new Country(GERMANY));
-//	private static final Book ULYSSES = new Book(1l, AUTHORS, "ULYSSES", RED_SEA_GERMANY, LocalDate.of(1919, 3, 15),
-//			30., new ArrayList<BookLogs>());
-//	private static final Book EXILES = new Book(1l, AUTHORS, "Exiles and poetry", RED_SEA_GERMANY, LocalDate.of(1918, 1, 1),
-//			30., new ArrayList<BookLogs>());
-//	private static final BookLogs log1 = new BookLogs(LocalDate.now(), LocalTime.now(), UserActions.STATUS_CHANGE, "test_log");
-//	
 	private static final UserItem ALYSSA = new UserItem();
-	
-	{
-		
-		
-	}
-
 	
 	@PersistenceContext 
 	private EntityManager em;
 	
 	@Autowired
 	UserRepository userRepo;
-
 
 	@Before
 	public void clear() {
@@ -64,9 +42,7 @@ public class UserTest {
 	
 	@Before 
 	public void buildUser() {
-		
 		ALYSSA.setFirstName("Alyssa");
-		
 	}
 
 	
@@ -78,13 +54,11 @@ public class UserTest {
 	public void addUser() {
 		UserItem createdUser = ALYSSA;
 		
-
 		userRepo.save(createdUser);
 		userRepo.flush();
 
-
 		UserItem persistedUser = userRepo.findById(createdUser.getId()).get();
-		System.out.println(persistedUser);
+
 		assertTrue(persistedUser.equals(createdUser));
 	}
 }

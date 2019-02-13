579b1a325aa73ef5951ae370e005a8bb5f52c958
mrcolombo1985 <mrcolombo1985@gmail.com>
2019-02-13 22:07:36 +0200

    13022019

--- MishpahugHaifa/src/main/java/Application/repo/EventRepository.java
+++ MishpahugHaifa/src/main/java/Application/repo/EventRepository.java
@@ -3,4 +3,9 @@ package Application.repo;
 import Application.entities.EventItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EventRepository extends JpaRepository<EventItem, Integer> {}
+import java.util.HashMap;
+import java.util.List;
+
+public interface EventRepository extends JpaRepository<EventItem, Integer> {
+    public List<EventItem> searchByFilter(HashMap<String, String> filter);
+}
--- MishpahugHaifa/src/main/java/Application/repo/UserRepository.java
+++ MishpahugHaifa/src/main/java/Application/repo/UserRepository.java
@@ -3,4 +3,9 @@ package Application.repo;
 import Application.entities.UserItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserItem, Integer> {}
+import java.util.HashMap;
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<UserItem, Integer> {
+    public List<UserItem> searchByFilter(HashMap<String, String> filter);
+}

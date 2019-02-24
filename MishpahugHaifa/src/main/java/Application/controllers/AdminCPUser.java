c13152f213d178fbf8b604b0cf28d26bb667efbf
mrcolombo1985 <45918270+mrcolombo1985@users.noreply.github.com>
2019-02-24 16:53:22 +0200

    controller AdminUser

--- MishpahugHaifa/src/main/java/Application/controllers/AdminCPUser.java
+++ MishpahugHaifa/src/main/java/Application/controllers/AdminCPUser.java
@@ -3,9 +3,9 @@ package Application.controllers;
 import Application.entities.UserEntity;
 import Application.models.user.IUserModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -13,7 +13,28 @@ import java.util.List;
 public class AdminCPUser {
     @Autowired
     IUserModel userModel;
+    @GetMapping(value="/getall")
     public List<UserEntity> getAll(){
         return userModel.getAll();
+    }
+    @PostMapping(value = "/add")
+    public UserEntity add(@RequestBody UserEntity data){
+        return userModel.add(data);
+    }
+    @PostMapping(value = "/update")
+    public UserEntity update(@RequestBody HashMap<String, String> data,@RequestParam(value = "id") Integer id){
+        return userModel.updateUser(id, data);
+    }
+    @DeleteMapping(value = "/remove")
+    public UserEntity remove(@RequestParam(value = "id") Integer id){
+        return userModel.remomeUser(id);
+    }
+    @GetMapping(value="/get")
+    public UserEntity get(@RequestParam(value = "id") Integer id){
+        return userModel.getById(id);
+    }
+    @GetMapping(value="/getbyfilter")
+    public List<UserEntity> getByFilter(@RequestBody HashMap<String, String> filter){
+        return userModel.getByFilter(filter);
     }
 }

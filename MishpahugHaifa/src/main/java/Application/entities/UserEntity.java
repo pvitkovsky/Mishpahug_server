27c2f503b718f02d7b450b72fd11e960ebe1d622
Philipp Vitkovsky <philipp.vitkovsky@gmail.com>
2019-03-05 16:24:02 +0200

    Testing before merge; typo

--- MishpahugHaifa/src/main/java/Application/entities/UserEntity.java
+++ MishpahugHaifa/src/main/java/Application/entities/UserEntity.java
@@ -29,6 +29,7 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import Application.entities.values.PictureValue;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;

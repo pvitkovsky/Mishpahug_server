a99c3a7408620010c3c7156404d75a452fbb7963
mrcolombo1985 <45918270+mrcolombo1985@users.noreply.github.com>
2019-03-22 11:21:23 +0200

    not need error

--- MishpahugHaifa/src/main/java/application/repositories/PasswordRepository.java
+++
@@ -1,9 +0,0 @@
-package application.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import application.entities.PasswordEntity;
-
-public interface PasswordRepository extends JpaRepository<PasswordEntity, Long> {
-
-}

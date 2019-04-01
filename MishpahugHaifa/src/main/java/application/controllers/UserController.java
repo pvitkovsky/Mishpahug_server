ec2630d886f4450f130c02d6ef3750e6e5c391ea
pvitkovsky <vitkovsky1337@gmail.com>
2019-04-01 19:40:45 +0300

    Cleanup after merge

--- MishpahugHaifa/src/main/java/application/controllers/UserController.java
+++ MishpahugHaifa/src/main/java/application/controllers/UserController.java
@@ -1,14 +1,14 @@
 package application.controllers;
 
 import java.util.HashMap;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.querydsl.binding.QuerydslPredicate;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.format.annotation.DateTimeFormat.ISO;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -54,7 +54,7 @@ public class UserController {
 
     @Autowired
     IHolyDayModel holyDayModel;
-`
+
 //TODO: clarify please
 //    @GetMapping(value="/{action}")
 //    public void getDataForAddForm(@PathVariable(value = "action", required = false) String action){

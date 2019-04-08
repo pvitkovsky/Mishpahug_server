0c847664c6994457c4d996592ab47e8702d0184f
mrcolombo1985 <45918270+mrcolombo1985@users.noreply.github.com>
2019-04-08 15:27:15 +0300

    Update AuthorizationFilter.java

--- MishpahugHaifa/src/main/java/application/configurations/AuthorizationFilter.java
+++ MishpahugHaifa/src/main/java/application/configurations/AuthorizationFilter.java
@@ -1,11 +1,13 @@
 package application.configurations;
 
+import application.entities.UserSession;
 import application.repositories.UserSessionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
@@ -21,11 +23,24 @@ public class AuthorizationFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest servletRequest = (HttpServletRequest) request;
-
-        System.out.println("Filter");
-        chain.doFilter(request, response);
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        if (request.getRequestURI().equals("/register") || request.getRequestURI().equals("/login")) {
+            chain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+        String token = request.getHeader("Authorization");
+        if (token == null) {
+            response.sendError(401);
+            return;
+        }
+        UserSession userSession = userSessionRepository.findByTokenAAndIsValidTrue(token);
+        if (userSession == null) {
+            response.sendError(401);
+            return;
+        }
+        chain.doFilter(servletRequest, servletResponse);
     }
 
     @Override

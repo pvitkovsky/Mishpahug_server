65e5c12187d4f4085143ea39907203f3ec9caf3e
mrcolombo1985 <45918270+mrcolombo1985@users.noreply.github.com>
2019-04-08 11:26:15 +0300

    Create AuthorizationFilter.java

---
+++ MishpahugHaifa/src/main/java/application/configurations/AuthorizationFilter.java
@@ -0,0 +1,22 @@
+package application.configurations;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class AuthorizationFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}

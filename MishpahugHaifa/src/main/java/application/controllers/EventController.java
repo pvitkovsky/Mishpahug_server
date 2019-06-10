395ebffa8671066b91855244d9357a6e4f65ef63
Philipp Vitkovsky <vitkovsky1337@gmail.com>
2019-06-10 20:50:13 +0300

    Preparation of controllers for the aspect-oriented refactoring

--- MishpahugHaifa/src/main/java/application/controllers/EventController.java
+++ MishpahugHaifa/src/main/java/application/controllers/EventController.java
@@ -29,153 +29,114 @@ import java.util.Set;
 @RequestMapping(value = "/event")
 public class EventController implements IEventController {
 
-    @Autowired
-    IEventModel eventModel;
+	@Autowired
+	IEventModel eventModel;
 
-    @Autowired
-    IConverter<EventEntity, EventDTO> converter;
+	@Autowired
+	IConverter<EventEntity, EventDTO> converter;
 
-    @Autowired
-    IConverter<UserEntity, UserDTO> converterUser;
+	@Autowired
+	IConverter<UserEntity, UserDTO> converterUser;
 
-    @Autowired
-    IHolyDayModel holyDayModel;
+	@Autowired
+	IHolyDayModel holyDayModel;
 
-    @Autowired
-    IKichenTypeModel kichenTypeModel;
+	@Autowired
+	IKichenTypeModel kichenTypeModel;
 
-    
-    //TODO:  events by owner;  events by subcscriber; check that no wrapping is done;  
-    
-    /* (non-Javadoc)
-     * @see application.controllers.intarfaces.IEventController#get(com.querydsl.core.types.Predicate)
-     */
-    @Override
-    @RequestMapping(method = RequestMethod.GET, value = "/")
-    @ResponseBody
-    public List<EventDTO> findAllByWebQuerydsl(@QuerydslPredicate(root = EventEntity.class) Predicate predicate,
-                                               @RequestHeader HttpHeaders httpHeaders,
-                                               HttpServletRequest request){
-        httpHeaders.forEach((key, value) -> {
-            log.info("EventController -> get -> headers -> " + String.format("Header '%s' = %s", key, value));
-        });
-        log.info("EventController -> get -> Remote IP -> " + request.getRemoteAddr());
-        return converter.DTOListFromEntities(eventModel.getAll(predicate));
-    }
+	@Override
+	@RequestMapping(method = RequestMethod.GET, value = "/")
+	@ResponseBody
+	public List<EventDTO> findAllByWebQuerydsl(@RequestHeader HttpHeaders httpHeaders, HttpServletRequest request,
+			@QuerydslPredicate(root = EventEntity.class) Predicate predicate) {
+		httpHeaders.forEach((key, value) -> {
+			log.info("EventController -> get -> headers -> " + String.format("Header '%s' = %s", key, value));
+		});
+		log.info("EventController -> get -> Remote IP -> " + request.getRemoteAddr());
+		return converter.DTOListFromEntities(eventModel.getAll(predicate));
+	}
 
-    /* (non-Javadoc)
-     * @see application.controllers.intarfaces.IEventController#findAll(java.lang.Integer)
-     */
-    @Override
-    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
-    @ResponseBody
-    public EventDTO findById(@PathVariable(name = "id") Integer id,
-                             @RequestHeader HttpHeaders httpHeaders,
-                             HttpServletRequest request){
-        httpHeaders.forEach((key, value) -> {
-            log.info("EventController -> findById -> headers -> " + String.format("Header '%s' = %s", key, value));
-        });
-        log.info("EventController -> findById -> Remote IP -> " + request.getRemoteAddr());
-        return new EventDTO(eventModel.getById(id));
-    }
+	@Override
+	@RequestMapping(method = RequestMethod.GET, value = "/{id}")
+	@ResponseBody
+	public EventDTO findById(@RequestHeader HttpHeaders httpHeaders, HttpServletRequest request,
+			@PathVariable(name = "id") Integer id) {
+		httpHeaders.forEach((key, value) -> {
+			log.info("EventController -> findById -> headers -> " + String.format("Header '%s' = %s", key, value));
+		});
+		log.info("EventController -> findById -> Remote IP -> " + request.getRemoteAddr());
+		return new EventDTO(eventModel.getById(id));
+	}
 
-    @RequestMapping(method = RequestMethod.GET, value = "/{id}/guests")
-    @ResponseBody
-    public List<UserDTO> findGuestByEventId(@PathVariable(name = "id") Integer id,
-                                            @RequestHeader HttpHeaders httpHeaders,
-                                            HttpServletRequest request){
-        httpHeaders.forEach((key, value) -> {
-            log.info("EventController -> findGuestByEventId -> headers -> " + String.format("Header '%s' = %s", key, value));
-        });
-        log.info("EventController -> findGuestByEventId -> Remote IP -> " + request.getRemoteAddr());
-        Set<SubscriptionEntity> subscriptionEntityList = eventModel.getById(id).getSubscriptions();
-        List<UserEntity> userEntityList = new ArrayList<>();
-        for (SubscriptionEntity x:subscriptionEntityList
-             ) {
-            userEntityList.add(x.getGuest());
-        }
-        return converterUser.DTOListFromEntities(userEntityList);
-    }
+	@RequestMapping(method = RequestMethod.GET, value = "/{id}/guests")
+	@ResponseBody
+	@Override
+	public List<UserDTO> findGuestByEventId(@RequestHeader HttpHeaders httpHeaders, HttpServletRequest request,
+			@PathVariable(name = "id") Integer id) {
+		httpHeaders.forEach((key, value) -> {
+			log.info("EventController -> findGuestByEventId -> headers -> "
+					+ String.format("Header '%s' = %s", key, value));
+		});
+		log.info("EventController -> findGuestByEventId -> Remote IP -> " + request.getRemoteAddr());
+		Set<SubscriptionEntity> subscriptionEntityList = eventModel.getById(id).getSubscriptions();
+		List<UserEntity> userEntityList = new ArrayList<>();
+		for (SubscriptionEntity x : subscriptionEntityList) {
+			userEntityList.add(x.getGuest());
+		}
+		return converterUser.DTOListFromEntities(userEntityList);
+	}
 
-    @Override
-    @GetMapping(value = "/byowner/{ownerusername}")
-    public List<EventDTO> getByOwner(@PathVariable(value = "ownerusername") String ownerusername,
-                                     @RequestHeader HttpHeaders httpHeaders,
-                                     HttpServletRequest request){
-        httpHeaders.forEach((key, value) -> {
-            log.info("EventController -> getByOwner -> headers -> " + String.format("Header '%s' = %s", key, value));
-        });
-        log.info("EventController -> getByOwner -> Remote IP -> " + request.getRemoteAddr());
-        return converter.DTOListFromEntities(eventModel.getByOwner(ownerusername));
-    }
+	@Override
+	@PostMapping(value = "/")
+	public EventDTO setDataFromForm(@RequestHeader HttpHeaders httpHeaders, HttpServletRequest request,
+			@RequestBody EventDTO data) {
+		EventEntity eventEntity = new EventEntity();
+		eventEntity.convertEventDTO(data);
+		eventEntity.setKitchenType(kichenTypeModel.getByName(data.getKichenType()));
+		eventEntity.setHoliDay(holyDayModel.getByName(data.getHoliday()));
+		httpHeaders.forEach((key, value) -> {
+			log.info("EventController -> setDataFromForm -> headers -> "
+					+ String.format("Header '%s' = %s", key, value));
+		});
+		log.info("EventController -> setDataFromForm -> Remote IP -> " + request.getRemoteAddr());
+		return new EventDTO(eventModel.add(eventEntity));
+	}
 
+	@Override
+	@PutMapping(value = "/{id}")
+	public EventDTO updateDataFromForm(@RequestHeader HttpHeaders httpHeaders, HttpServletRequest request,
+			@RequestBody HashMap<String, String> data, @PathVariable(value = "id") Integer id) {
+		httpHeaders.forEach((key, value) -> {
+			log.info("EventController -> updateDataFromForm -> headers -> "
+					+ String.format("Header '%s' = %s", key, value));
+		});
+		data.forEach((key, value) -> {
+			log.info("EventController -> updateDataFromForm -> data -> " + String.format("data '%s' = %s", key, value));
+		});
+		log.info("EventController -> updateDataFromForm -> Remote IP -> " + request.getRemoteAddr());
+		return new EventDTO(eventModel.update(id, data));
+	}
 
-    /* (non-Javadoc)
-     * @see application.controllers.intarfaces.IEventController#setDataFromForm(application.dto.EventDTO)
-     */
-    @Override
-    @PostMapping(value = "/")
-    public EventDTO setDataFromForm(@RequestBody EventDTO data,
-                                    @RequestHeader HttpHeaders httpHeaders,
-                                    HttpServletRequest request){
-        EventEntity eventEntity = new EventEntity();
-        eventEntity.convertEventDTO(data);
-        eventEntity.setKitchenType(kichenTypeModel.getByName(data.getKichenType()));
-        eventEntity.setHoliDay(holyDayModel.getByName(data.getHoliday()));
-        httpHeaders.forEach((key, value) -> {
-            log.info("EventController -> setDataFromForm -> headers -> " + String.format("Header '%s' = %s", key, value));
-        });
-        log.info("EventController -> setDataFromForm -> Remote IP -> " + request.getRemoteAddr());
-        return new EventDTO(eventModel.add(eventEntity));
-    }
+	@Override
+	@DeleteMapping(value = "/{id}")
+	public void delete(@RequestHeader HttpHeaders httpHeaders, HttpServletRequest request,
+			@PathVariable(value = "id") Integer id) {
+		eventModel.getById(id).putIntoDeletionQueue();
+		httpHeaders.forEach((key, value) -> {
+			log.info("EventController -> delete -> headers -> " + String.format("Header '%s' = %s", key, value));
+		});
+		log.info("EventController -> delete -> Remote IP -> " + request.getRemoteAddr());
+		eventModel.delete(id);
+	}
 
-    /* (non-Javadoc)
-     * @see application.controllers.intarfaces.IEventController#updateDataFromForm(java.util.HashMap, java.lang.Integer)
-     */
-    @Override
-    @PutMapping(value = "/{id}")
-    public EventDTO updateDataFromForm(@RequestBody HashMap<String, String> data,
-                                       @PathVariable(value = "id") Integer id,
-                                       @RequestHeader HttpHeaders httpHeaders,
-                                       HttpServletRequest request){
-        httpHeaders.forEach((key, value) -> {
-            log.info("EventController -> updateDataFromForm -> headers -> " + String.format("Header '%s' = %s", key, value));
-        });
-        data.forEach((key, value) -> {
-            log.info("EventController -> updateDataFromForm -> data -> " + String.format("data '%s' = %s", key, value));
-        });
-        log.info("EventController -> updateDataFromForm -> Remote IP -> " + request.getRemoteAddr());
-        return new EventDTO(eventModel.update(id, data));
-    }
-
-    /* (non-Javadoc)
-     * @see application.controllers.intarfaces.IEventController#delete(java.lang.Integer)
-     */
-    @Override
-    @DeleteMapping(value = "/{id}")
-    public void delete(@PathVariable(value = "id") Integer id,
-                       @RequestHeader HttpHeaders httpHeaders,
-                       HttpServletRequest request){
-        eventModel.getById(id).putIntoDeletionQueue();
-        httpHeaders.forEach((key, value) -> {
-            log.info("EventController -> delete -> headers -> " + String.format("Header '%s' = %s", key, value));
-        });
-        log.info("EventController -> delete -> Remote IP -> " + request.getRemoteAddr());
-        eventModel.delete(id);
-    }
-
-    /* (non-Javadoc)
-     * @see application.controllers.intarfaces.IEventController#delete()
-     */
-    @Override
-    @DeleteMapping(value = "/")
-    public void delete(@RequestHeader HttpHeaders httpHeaders,
-                       HttpServletRequest request){
-        httpHeaders.forEach((key, value) -> {
-            log.info("EventController -> delete -> headers -> " + String.format("Header '%s' = %s", key, value));
-        });
-        log.info("EventController -> delete -> Remote IP -> " + request.getRemoteAddr());
-        eventModel.getAll().forEach(EventEntity::putIntoDeletionQueue);
-        eventModel.deleteAll();
-    }
+	@Override
+	@DeleteMapping(value = "/")
+	public void delete(@RequestHeader HttpHeaders httpHeaders, HttpServletRequest request) {
+		httpHeaders.forEach((key, value) -> {
+			log.info("EventController -> delete -> headers -> " + String.format("Header '%s' = %s", key, value));
+		});
+		log.info("EventController -> delete -> Remote IP -> " + request.getRemoteAddr());
+		eventModel.getAll().forEach(EventEntity::putIntoDeletionQueue);
+		eventModel.deleteAll();
+	}
 }
